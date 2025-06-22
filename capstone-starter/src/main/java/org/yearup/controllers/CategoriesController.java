3942e82a997ea1bc123be8f34ce0d205e8440d0a
faith spikes <faithspikes3@gmail.com>
2025-06-22 12:50:51 -0700

    Get all categories and get categories by id

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,39 +1,48 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // add the annotations to make this a REST controller
+@RestController
 // add the annotation to make this controller the endpoint for the following url
+@RequestMapping("/categories")
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
-public class CategoriesController
-{
+@CrossOrigin
+public class CategoriesController {
+
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao  productDao){
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping
+    public List<Category> getAll() {// find and return all categories
+
+        return categoryDao.getAllCategories();
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
     public Category getById(@PathVariable int id)
-    {
-        // get the category by id
-        return null;
+    {// get the category by id
+
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,35 +1,66 @@
 package org.yearup.data.mysql;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.yearup.data.mysql.MySqlProductDao.mapRow;
+
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
+    public List<Category> getAllCategories() {// get all categories
+        List<Category> categories = new ArrayList<>();
+
+        String sql = "SELECT * FROM categories;";
+        try (
+                Connection connection = getConnection();
+                PreparedStatement psm = connection.prepareStatement(sql);
+                ResultSet rs = psm.executeQuery()) {
+            while (rs.next()) {
+                Category category = mapRow(rs);
+                categories.add(category);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("There was an error: " + e.getMessage());
+
+        }
+        return categories;
     }
 
     @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
-        return null;
+    public Category getById(int categoryId) {// get category by id
+        String sql = "SELECT * FROM categories WHERE category_id = ?";
+        try (
+                Connection conn = getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+
+            ps.setInt(1, categoryId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+                System.out.println("Found Category with ID: " + categoryId);
+            return mapRow(rs);
+        }
+        catch (SQLException e){
+            System.out.println("ERROR ALERT: " + e.getMessage());
     }
+        return null;//if no match found
+}
 
     @Override
     public Category create(Category category)
